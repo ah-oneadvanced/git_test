8a2969979f5dfe492557d2600f30282fb3e0b23e
hishobkar1 <abhijeet.hishobkar@oneadvanced.com>
2025-11-21 16:09:27 +0530

    Initial commit: Basic calculator app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConflictPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Conflict Resolution Practice!");
+            Console.WriteLine("Version: 1.0");
+            
+            // Simple calculator
+            int a = 10;
+            int b = 5;
+            
+            Console.WriteLine($"Addition: {a} + {b} = {a + b}");
+            Console.WriteLine($"Subtraction: {a} - {b} = {a - b}");
+        }
+    }
+}
